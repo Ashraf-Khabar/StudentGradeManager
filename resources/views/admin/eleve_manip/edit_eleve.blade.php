855c96efa3951298016d5877beac2466c5072508
ashraf khabar <khabarachraf@gmail.com>
2022-05-20 00:28:07 +0100

    Update edit_eleve.blade.php

--- resources/views/admin/eleve_manip/edit_eleve.blade.php
+++ resources/views/admin/eleve_manip/edit_eleve.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/assets/css/styles.min.css?h=620c8c37aafe1fc4766350dfc53ccc7d">
-    <link rel="canonical" href="https://icons.getbootstrap.com/icons/check/">
+
     <link rel="stylesheet" href="/assets/css/docs.css">
 
     <title>edit eleve</title>
@@ -20,7 +20,7 @@
                 <center>
                     @auth
                         @if (session('status'))
-                            <div><i class="bi bi-check"></i> succefully</div>
+                            <div style="color: red; font-size: 30px"><i class="bi bi-check"></i> succefully</div>
                         @endif
                         <form action="{{url('Eleves/'.$eleve->id)}}" method="POST">
 
