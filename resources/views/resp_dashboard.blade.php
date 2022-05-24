87f5b2fdd9169b6f5078f7ac57f39061eb9bd521
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2022-05-24 15:45:43 +0100

    commit

--- resources/views/resp_dashboard.blade.php
+++ resources/views/resp_dashboard.blade.php
@@ -4,21 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
+    <link rel="stylesheet" href="/assets/css/styles.min.css?h=820568f5bc17b2eadf42d912a6587674">
     <title>eleve</title>
 </head>
 <body>
-    <h3>hello resp</h3>
-    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-        Logout
-    </a>
-    
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        {{ csrf_field() }}
-    </form>
+<section>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{route('welcome')}}">
+                <img src="../assets/img/monLogo.png" width="70px">
+            </a>
+            <button style="background-color: #dc3855" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon "></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a style="color: #dc3855" class="nav-link active" href="/messages">Messages</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a style="color: #dc3855" class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Modify
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="{{url('Modules')}}">Modify Module</a></li>
+                            <li><a class="dropdown-item" href="{{url('Element_Modules')}}">modify Element de module</a></li>
+                            <li><a class="dropdown-item" href="{{url('Notes')}}">modify Notes</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
 
-    <br><a href="{{url('Modules')}}">modify modules</a>
-    <br><a href="{{url('Element_Modules')}}">modify elements</a>
-    <br><a href="{{url('Notes')}}">modify notes</a>
+                        <a style="color: #dc3855" class="nav-link active" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
